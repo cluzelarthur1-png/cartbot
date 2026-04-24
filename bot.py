e48cf9a5f775cad8680837327525fa146c075ca4
cluzelarthur1-png <cluzelarthur1@gmail.com>
2026-04-24 04:05:27 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from datetime import datetime, timezone
 
 # ─── CONFIG ───────────────────────────────────────────────────────────────────
 CART_CHANNEL_NAME    = "cartbot"
-CLAIM_CHANNEL_NAME   = "🎟️ wts carts"
+CLAIM_CHANNEL_NAME   = "🎟️-wts-carts"
 TICKET_CATEGORY_NAME = "Tickets"
 ADMIN_ROLE_NAMES     = ["Admin", "owner", "runner"]  # tous les rôles autorisés
 CONFIG_FILE          = "config.json"
